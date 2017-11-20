8897cfccf57baa53b4434558bd3d19bb023aaa33
MrSwirlyEyes <ProfSwirlyEyes@gmail.com>
2017-11-20 12:10:31 -0800

    Initialized ECE 16 CurieBLE repo.  Central uses IMU to write values to Peripheral.  Peripheral then prints IMU readings.  Peripheral Explorer is just to demonstrate that you can find our Peripheral IMU Service

---
+++ ece16_central/ece16_central.ino
@@ -0,0 +1,93 @@
+#include <CurieBLE.h>
+
+#include <CurieIMU.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // Initialize IMU
+  CurieIMU.begin();
+  Serial.println("IMU initialized...");
+
+  // Set Accelerometer range
+  CurieIMU.setAccelerometerRange(2);
+
+  // Initialize the CurieBLE
+  BLE.begin();
+
+  Serial.println("BLE Accelerometer Central");
+
+  // Scan/look-for a Peripheral
+  BLE.scanForUuid("10000000-0000-0000-0000-000000000000");
+}
+
+void loop() {
+  // See if we found the Accelerometer Peripheral
+  BLEDevice peripheral=BLE.available();
+
+  // If a peripheral has been connected to:
+  if(peripheral) {
+    // We found the peripheral we were looking for!
+    Serial.print("Discovered ");
+    Serial.print(peripheral.address());
+    Serial.print(" '");
+    Serial.print(peripheral.localName());
+    Serial.print("' ");
+    Serial.print(peripheral.advertisedServiceUuid());
+    Serial.println();
+
+    // No need to keep looking for anything else now
+    BLE.stopScan();
+
+    
+
+    // Write Accelerometer readings to Peripheral's Accelerometer Service + Characteristics
+    write_to_peripheral(peripheral);
+
+    // If we disconnected with Peripheral, try again!
+    BLE.scanForUuid("10000000-0000-0000-0000-000000000000");
+  }
+}
+
+void write_to_peripheral(BLEDevice peripheral) {
+  // Connect to Peripheral
+    Serial.println("Connecting...");
+
+    if(peripheral.connect()) {
+      Serial.println("Connected!");
+    } else {
+      Serial.println("Failed to connect!");    
+      return; 
+    }
+
+    // See if our Peripheral has Characteristics/Attributes
+    Serial.println("Discovering Characteristics/Attributes ...");
+    if (peripheral.discoverAttributes()) {
+      Serial.println("Characteristics/Attributes discovered");
+    } else {
+      Serial.println("Characteristic/Attribute discovery failed!");
+      peripheral.disconnect();      
+      return;  
+    }
+    
+  // Retrieve the Accelerometer Characteristic(s) from Peripheral
+  BLECharacteristic accelerometer_characteristic_x=peripheral.characteristic("10000000-0000-0000-0000-000000000001");
+  BLECharacteristic accelerometer_characteristic_y=peripheral.characteristic("10000000-0000-0000-0000-000000000002");
+  BLECharacteristic accelerometer_characteristic_z=peripheral.characteristic("10000000-0000-0000-0000-000000000003");
+
+  // While we are still connected to the Peripheral, update its IMU Characteristics
+  while(peripheral.connected()) {
+    int x=CurieIMU.readAccelerometer(X_AXIS);
+    accelerometer_characteristic_x.writeInt(x);
+
+    int y=CurieIMU.readAccelerometer(Y_AXIS);
+    accelerometer_characteristic_y.writeInt(y);
+
+    int z=CurieIMU.readAccelerometer(Z_AXIS);
+    accelerometer_characteristic_z.writeInt(z);
+  }
+
+  Serial.println("Peripheral disconnected!");
+}
+
---
+++ ece16_peripheral/ece16_peripheral.ino
@@ -0,0 +1,72 @@
+#include <CurieBLE.h>
+
+// BLE Accelerometer Service
+BLEService accelerometer_service("10000000-0000-0000-0000-000000000000");
+
+// BLE Accelerometer Service Characteristics
+BLEIntCharacteristic accelerometer_characteristic_x("10000000-0000-0000-0000-000000000001", BLERead | BLEWrite);
+BLEIntCharacteristic accelerometer_characteristic_y("10000000-0000-0000-0000-000000000002", BLERead | BLEWrite);
+BLEIntCharacteristic accelerometer_characteristic_z("10000000-0000-0000-0000-000000000003", BLERead | BLEWrite);
+
+
+void setup() {
+  // Start Serial Monitor
+  Serial.begin(9600);
+
+  // Initialize the CurieBLE
+  BLE.begin();
+
+  // Set a name for the Service we are broadcasting
+  BLE.setLocalName("Accelerometer");
+
+  // Advertise this Service
+  BLE.setAdvertisedService(accelerometer_service);
+
+  // Add Characteristics to the Service
+  accelerometer_service.addCharacteristic(accelerometer_characteristic_x);
+  accelerometer_service.addCharacteristic(accelerometer_characteristic_y);
+  accelerometer_service.addCharacteristic(accelerometer_characteristic_z);
+  
+  // Add the Service
+  BLE.addService(accelerometer_service);
+
+  // Set initial Value for Characteristics
+  accelerometer_characteristic_x.setValue(0);
+  accelerometer_characteristic_y.setValue(0);
+  accelerometer_characteristic_z.setValue(0);
+  
+
+  // Start the broadcast
+  BLE.advertise();
+
+  Serial.println("BLE Accelerometer Peripheral");
+}
+
+void loop() {
+  // Set up a Central to connect to us/Peripheral
+  BLEDevice central=BLE.central();
+
+  // If a central has connected to us/Peripheral:
+  if(central) {
+    Serial.print("A central has connected: ");
+    Serial.println(central.address());
+
+    // While Central is still connected to us/Peripheral:
+    while(central.connected()) {
+  
+      // Check if Central has updated/written a new value for Characteristic(s)
+      if(accelerometer_characteristic_x.written() ||
+          accelerometer_characteristic_y.written() ||
+          accelerometer_characteristic_z.written()) {
+        Serial.print("(");
+        Serial.print(accelerometer_characteristic_x.value()); Serial.print(",");
+        Serial.print(accelerometer_characteristic_y.value()); Serial.print(",");
+        Serial.print(accelerometer_characteristic_z.value()); Serial.println(")");
+      }
+    }
+  
+    // Otherwise, Central has disconnected!
+    Serial.print("Central has disconnected: ");
+    Serial.println(central.address());
+  }
+}
